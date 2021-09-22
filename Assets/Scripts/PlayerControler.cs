9a8775a82d8e2f6cb15ca2445b65c1213c8cea9c
xtopala <xtopala@gmail.com>
2021-09-22 21:04:15 +0200

    finished controlling the player

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
@@ -9,10 +7,16 @@ public class PlayerControler : MonoBehaviour
 
     public Rigidbody2D theRB;
 
+    public Transform gunArm;
+
+    private Camera theCam;
+
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theCam = Camera.main;
     }
 
     // Update is called once per frame
@@ -21,8 +25,38 @@ public class PlayerControler : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
+        moveInput.Normalize();
+
         //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
 
         theRB.velocity = moveInput * moveSpeed;
+
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+        if (mousePosition.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunArm.localScale = Vector3.one;
+        }
+
+        // rotate gun arm
+        Vector2 offset = new Vector2(mousePosition.x - screenPoint.x, mousePosition.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        gunArm.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
     }
 }
