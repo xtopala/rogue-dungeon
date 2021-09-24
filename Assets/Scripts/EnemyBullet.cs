8facf89a50202bb824e7c7afb22cbbf2cc986b81
xtopala <xtopala@gmail.com>
2021-09-24 17:41:22 +0200

    added Player Health system

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -24,6 +24,7 @@ public class EnemyBullet : MonoBehaviour
         if (collision.tag == "Player")
         {
             // damage player
+            PlayerHealthController.instance.DamagePlayer();
         }
 
         Destroy(gameObject);
---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+
+    public int currentHealth;
+    public int maxHealth;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DamagePlayer()
+    {
+        currentHealth--;
+
+        if (currentHealth <= 0)
+        {
+            PlayerController.instance.gameObject.SetActive(false);
+        }
+    }
+}
