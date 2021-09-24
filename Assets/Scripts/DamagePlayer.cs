066dfee5671ce147d2d0dcd2ea564770720876a6
xtopala <xtopala@gmail.com>
2021-09-24 22:09:43 +0200

    added Static Dangers

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerHealthController.instance.DamagePlayer();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerHealthController.instance.DamagePlayer();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerHealthController.instance.DamagePlayer();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerHealthController.instance.DamagePlayer();
+        }
+    }
+}
