24f84e80a8e23e58af944f0bcee6aef36a030dc8
xtopala <xtopala@gmail.com>
2021-09-26 19:26:11 +0200

    unlocking doors when room is cleared

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController instance;
+
+    public float moveSpeed;
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), moveSpeed * Time.deltaTime);
+        }
+    }
+
+    public void ChangeTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+}
