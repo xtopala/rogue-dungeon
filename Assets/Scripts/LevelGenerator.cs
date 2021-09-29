f763a4c8666b631e3f71284d81147d6cba839275
xtopala <xtopala@gmail.com>
2021-09-29 10:32:02 +0200

    genarating rooms, stoping them overlapp

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject layoutRoom;
+    public Color startColor, endColor;
+
+    public int distanceToEnd;
+
+    public Transform generatorPoint;
+
+    public enum Direction { up, right, down, left };
+    public Direction selectedDirection;
+
+    public float xOffset = 18f, yOffset = 10f;
+
+    public LayerMask whatIsRoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+
+        selectedDirection = (Direction)Random.Range(0, 4);
+        MoveGenerationPoint();
+
+        for (int i = 0; i < distanceToEnd; i++)
+        {
+            Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation);
+            selectedDirection = (Direction)Random.Range(0, 4);
+            MoveGenerationPoint();
+
+            while (Physics2D.OverlapCircle(generatorPoint.position, .2f, whatIsRoom))
+            {
+                MoveGenerationPoint();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void MoveGenerationPoint()
+    {
+        switch (selectedDirection)
+        {
+            case Direction.up:
+                generatorPoint.position += new Vector3(0f, yOffset, 0f);
+                break;
+
+            case Direction.down:
+                generatorPoint.position += new Vector3(0f, -yOffset, 0f);
+                break;
+
+            case Direction.right:
+                generatorPoint.position += new Vector3(xOffset, 0f, 0f);
+                break;
+
+            case Direction.left:
+                generatorPoint.position += new Vector3(-xOffset, 0f, 0f);
+                break;
+        }
+    }
+}
