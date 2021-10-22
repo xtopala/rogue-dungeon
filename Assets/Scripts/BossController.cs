acd8b20466002dfd4e7869e17780ff6054183337
xtopala <xtopala@gmail.com>
2021-10-22 11:53:49 +0200

    makind Actions happen

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -5,6 +5,12 @@ public class BossController : MonoBehaviour
     public static BossController instance;
 
     public BossAction[] actions;
+    private int currentAction;
+    private float actionCounter;
+
+    private float shotCounter;
+    private Vector2 moveDirection;
+    public Rigidbody2D theRB;
 
     private void Awake()
     {
@@ -14,13 +20,59 @@ public class BossController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        actionCounter = actions[currentAction].actionLength;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (actionCounter > 0)
+        {
+            actionCounter -= Time.deltaTime;
 
+            // handle movement
+            moveDirection = Vector2.zero;
+
+            if (actions[currentAction].shouldMove)
+            {
+                if (actions[currentAction].shouldChasePlayer)
+                {
+                    moveDirection = PlayerController.instance.transform.position - transform.position;
+                    moveDirection.Normalize();
+                }
+
+                if (actions[currentAction].moveToPoint)
+                {
+                    moveDirection = actions[currentAction].pointToMoveTo.position - transform.position;
+                }
+            }
+
+            theRB.velocity = moveDirection * actions[currentAction].moveSpeed;
+
+            // handle shooting
+            if (actions[currentAction].shouldShoot)
+            {
+                shotCounter -= Time.deltaTime;
+                if (shotCounter <= 0)
+                {
+                    shotCounter = actions[currentAction].timeBetweenShots;
+                    foreach (Transform t in actions[currentAction].shotPoints)
+                    {
+                        Instantiate(actions[currentAction].itemToShoot, t.position, t.rotation);
+                    }   
+                }
+            }
+        }
+        else
+        {
+            currentAction++;
+            if (currentAction >= actions.Length)
+            {
+                currentAction = 0;
+            }
+
+            actionCounter = actions[currentAction].actionLength;
+        }
     }
 }
 
